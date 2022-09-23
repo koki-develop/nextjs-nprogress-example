bb7f24bac2b0f850d219dc77f31100ca9cbd5ea5
koki-develop <kou.pg.0131@gmail.com>
2022-09-23 16:19:48 +0900

    nprogress を導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import nProgress from "nprogress";
+import { useCallback, useEffect } from "react";
+import "nprogress/nprogress.css";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+nProgress.configure({});
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  const handleRouteStart = useCallback(() => nProgress.start(), []);
+  const handleRouteEnd = useCallback(() => nProgress.done(), []);
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", handleRouteStart);
+    router.events.on("routeChangeComplete", handleRouteEnd);
+    router.events.on("routeChangeError", handleRouteEnd);
+    return () => {
+      router.events.off("routeChangeStart", handleRouteStart);
+      router.events.off("routeChangeComplete", handleRouteEnd);
+      router.events.off("routeChangeError", handleRouteEnd);
+    };
+  }, [handleRouteEnd, handleRouteStart, router.events]);
+
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
